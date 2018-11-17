5a02887ad4d8adc48bd906eddbe6755160c86f15
Никита Гревцов <n.a.grevtsov@gmail.com>
2018-11-17 13:37:18 +0700

    Добавлен прелоадер

--- app/js/main.js
+++ app/js/main.js
@@ -5,8 +5,13 @@ import render from './render.js';
 (function () {
 
     load.apps(data => {
+        let preloader = document.querySelector('template').content.querySelector('.preloader-chasing-squares');
+        document.querySelector('.c-packs__list').appendChild(preloader);
         let apps = JSON.parse(data.target.responseText);
         let cutApps = utils.getUniqueRandomElement(apps, 3);
-        render.sliderApps(cutApps);
+        setTimeout(() => {
+            render.sliderApps(cutApps);
+            preloader.remove();
+        }, 1000);
     });
 })();
