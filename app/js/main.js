f502fd396fc35821e74c96cb7ca921cf4d544a89
Никита Гревцов <n.a.grevtsov@gmail.com>
2018-11-24 12:36:28 +0700

    Добавлены условия, определяющие страницу

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,19 @@
 import * as load from './load.js';
 import * as render from './render.js';
 import * as slider from './slider.js'
-let preloader = document.querySelector('template').content.querySelector('.preloader-chasing-squares');
-document.querySelector('.c-packs__list').appendChild(preloader);
-load.loadApps(apps => {
-    setTimeout(() => {
-        render.sliderApps(apps);
-        preloader.remove();
-        let appSlider = new slider.Slider('.c-packs__list');
-    }, 1000);
-});
+
+if (document.querySelector('body').classList.contains('main')) {
+    let preloader = document.querySelector('template').content.querySelector('.preloader-chasing-squares');
+    document.querySelector('.c-packs__list').appendChild(preloader);
+    load.loadApps(apps => {
+        setTimeout(() => {
+            render.sliderApps(apps);
+            preloader.remove();
+            let appSlider = new slider.Slider('.c-packs__list');
+        }, 1000);
+    });
+}
+
+if (document.querySelector('body').classList.contains('app')) {
+
+}
