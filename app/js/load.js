57be06ea66fdcc37002d974214f79221da28ff7a
Никита Гревцов <n.a.grevtsov@gmail.com>
2018-11-24 14:30:27 +0700

    Правка функций get и loadApps

--- app/js/load.js
+++ app/js/load.js
@@ -2,19 +2,18 @@ import * as utils from "./utils.js";
 
 export function apps() {
     const APPS_URL = '/api/apps.json';
-    return get(APPS_URL)
-        .then((response) => {
-            let apps = JSON.parse(response);
+    return getJSON(APPS_URL)
+        .then((apps) => {
             return utils.getUniqueRandomElement(apps, 6);
         });
 }
 
-export function get(url) {
+export function getJSON(url) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
         xhr.onload = () => {
-            resolve(xhr.responseText);
+            resolve(JSON.parse(xhr.responseText));
         };
         xhr.onerror = () => {
             reject('Error');
@@ -22,18 +21,6 @@ export function get(url) {
         xhr.send();
     });
 }
-
-/*export function loadApps(loadHandler) {
-    const xhr = new XMLHttpRequest();
-    const APPS_URL = '/api/apps.json';
-    xhr.open('GET', APPS_URL, true);
-    xhr.send();
-    xhr.onload = function() {
-        let apps = JSON.parse(xhr.responseText);
-        let cutApps = utils.getUniqueRandomElement(apps, 6);
-        loadHandler(cutApps);
-    };
-}*/
 
 export function loadSingleApp(id, loadHandler) {
     const xhr = new XMLHttpRequest();
