aadd2193ca239015cda7e5f82582819243182c1c
Никита Гревцов <n.a.grevtsov@gmail.com>
2018-11-25 11:27:59 +0700

    Реализован рендер шапки приложения

--- app/js/app.js
+++ app/js/app.js
@@ -13,9 +13,17 @@ export class App {
     }
     _renderApp(data) {
         let appEl = document.querySelector('template').content.querySelector('.o-app-page').cloneNode(true);
-        appEl.querySelector('.o-app-page__title').innerHTML = data.title;
         let date = new Date(+data.date * 1000);
+        let header = data.headerText;
+        appEl.querySelector('.o-app-page__title').innerHTML = data.title;
         appEl.querySelector('.o-app-header__date').innerHTML = formatDate(date);
+        appEl.querySelector('.o-app-header__licensed').innerHTML = header.licensedText;
+        appEl.querySelector('.o-app-header__type').innerHTML = header.type;
+        appEl.querySelector('.o-app-header__developer').innerHTML = header.developer;
+        appEl.querySelector('.o-app-header__sku').innerHTML = header.sku;
+        appEl.querySelector('.o-app-header__requirements').innerHTML += header.requirements;
+        appEl.querySelector('.o-app-header__img').src = data.image;
+        appEl.querySelector('.js-add-to-cart').dataset.id = data.id;
 
         document.querySelector('.l-content__content').appendChild(appEl);
     }
